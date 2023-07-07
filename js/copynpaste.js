a986a25c8b0fa570936c892735e52c272d53f6d1
Chanyoung <cisalive@naver.com>
2023-07-07 16:02:23 +0900

    포트폴리오 내용보강. 애니메이션 효과 추가완료. 카톡 캡쳐 추가 필요.

---
+++ js/copynpaste.js
@@ -0,0 +1,10 @@
+const texts = document.querySelectorAll(".contacts .contact > p ");
+console.log(texts);
+Array.from(texts).forEach((text, idx)=>{
+    text.addEventListener("click", async () => {
+        // 클립보드 복사가 비동기적 작업이기 때문에, 비동기 작업 가능하게 하는 async와 지연시켜주는 await 키워드 사용
+        await navigator.clipboard.writeText(text.innerText);
+        let sort = ['전화번호가', 'URL이', '이메일주소가']
+        alert(`${sort[idx]} 클립보드에 복사되었습니다.`);
+    })
+});
--- js/fullpage.js
+++ js/fullpage.js
@@ -18,7 +18,6 @@ const anchors = document.querySelectorAll("#navis > li");
 window.addEventListener("wheel", (e)=>{
     let selectedAnchor = document.querySelector("#navis > li.active");
     let datas = selectedAnchor.getAttribute('data-menuanchor');
-    console.log(selectedAnchor, datas);
     (datas === 'intro') ? navis.classList.remove('on') : navis.classList.add('on');
 });
 
@@ -33,62 +32,3 @@ anchors.forEach((anchor)=>{
         (datas === 'intro') ? navis.classList.remove('on') : navis.classList.add('on');
     });
 })
-
-// 포폴 목록 메뉴
-const navis2 = document.querySelectorAll(".navis2 .wrap > li");
-// 포폴 이전, 다음 메뉴
-// const prevBtn = document.querySelector(".fp-prev");
-// const nextBtn = document.querySelector(".fp-next");
-// data-anchor 3개
-const locates = ["#portpolio", '#portpolio/jameson', '#portpolio/pinkgym']
-// 돌려쓸 숫자
-// let index = 0;
-
-// 메뉴 클릭시 스타일 변경
-// navis2.forEach((menu, idx)=>{
-//     menu.addEventListener("click", ()=>{
-//         navis2.forEach((el)=>{
-//             el.classList.remove("active");
-//         })
-//         menu.classList.add("active");
-//         index = idx;
-//     });
-// });
-
-// //이전, 이후 버튼 클릭 시 스타일 변경
-// prevBtn.addEventListener("click", ()=>{
-//     (index === 0) ? index = 2 : index--;
-//     navis2.forEach((el)=>{
-//         el.classList.remove("active");
-//     })
-//     navis2[index].classList.add("active");
-// });
-// nextBtn.addEventListener("click", ()=>{
-//     (index === 2) ? index = 0 : index++;
-//     navis2.forEach((el)=>{
-//         el.classList.remove("active");
-//     })
-//     navis2[index].classList.add("active");
-// });
-
-// window.addEventListener('hashchange', function() {
-//     const anchor = location.hash;
-//     if(anchor === locates[0]){
-//         navis2.forEach((el)=>{
-//             el.classList.remove("active");
-//         })
-//         navis2[0].classList.add("active");
-//     } 
-//     else if(anchor === locates[1]){
-//         navis2.forEach((el)=>{
-//             el.classList.remove("active");
-//         })
-//         navis2[1].classList.add("active");
-//     }
-//     else if(anchor === locates[2]){
-//         navis2.forEach((el)=>{
-//             el.classList.remove("active");
-//         })
-//         navis2[2].classList.add("active");
-//     }
-// });
