86ea21e129ecac3bcd664e10fef74b43e35c1e57
chanyoung0809 <cisalive@naver.com>
2023-07-16 21:24:43 +0900

    7월 16일자 수정본. 다크모드 구현, 포폴페이지 클릭시 페이지 및 이벤트 추가 필요

--- js/fullpage.js
+++ js/fullpage.js
@@ -2,7 +2,7 @@ new fullpage('#fullpage', {
     //options here
     autoScrolling:true,
     menu: '#navis',
-    anchors:['intro', 'about', 'strength', 'portpolio', 'contact'],
+    anchors:['intro', 'about', 'portfolio', 'strength', 'contact'],
     licenseKey: 'gplv3-license',
 	keyboardScrolling: true,
 	animateAnchor: true,
@@ -12,15 +12,28 @@ new fullpage('#fullpage', {
 });
 
 const navis = document.querySelector("#navis");
-const anchors = document.querySelectorAll("#navis > li");
+const anchors = document.querySelectorAll("#navis > li > a");
 
-// 휠 동작 시 메뉴 색상 변경
-window.addEventListener("wheel", (e)=>{
+// // 휠 동작 시 메뉴 색상 변경
+// window.addEventListener("wheel", (e)=>{
+//     let selectedAnchor = document.querySelector("#navis > li.active");
+//     let datas = selectedAnchor.getAttribute('data-menuanchor');
+//     (datas === 'intro') ? navis.classList.remove('on') : navis.classList.add('on');
+// });
+// 주소창 변경 시 메뉴 스타일 변경
+window.addEventListener("hashchange", () => {
     let selectedAnchor = document.querySelector("#navis > li.active");
     let datas = selectedAnchor.getAttribute('data-menuanchor');
-    (datas === 'intro') ? navis.classList.remove('on') : navis.classList.add('on');
+    
+    if (datas === 'intro') {
+      navis.classList.remove('on');
+    } 
+    else {
+      navis.classList.add('on');
+    }
+    
 });
-
+  
 // 하단 메뉴 클릭 시 메뉴 스타일, 색상 변경
 anchors.forEach((anchor)=>{
     anchor.addEventListener("click", ()=>{
--- js/swiper.js
+++ js/swiper.js
@@ -8,63 +8,96 @@ const swiper = new Swiper('.co_workers', {
       el: '.swiper-pagination',
       clickable: true,
     },
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
 });
-const swiper1 = new Swiper('.pSwiper1', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  grabCursor:true,
-  autoplay: {
-    delay: 5000,
+
+const portfolioSwiper = new Swiper(".portfolioSwiper", {
+  direction: 'horizontal',  
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "3",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
   },
+  initialSlide:1,
   // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
+  keyboard: {
+    enabled: true,
   },
+  // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
-const swiper2 = new Swiper('.pSwiper2', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  grabCursor:true,
-  autoplay: {
-    delay: 5000,
-  },
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-const swiper3 = new Swiper('.pSwiper3', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  grabCursor:true,
-  autoplay: {
-    delay: 5000,
-  },
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+
+// const swiper1 = new Swiper('.pSwiper1', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+//   grabCursor:true,
+//   autoplay: {
+//     delay: 5000,
+//   },
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//     type: "fraction",
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+// const swiper2 = new Swiper('.pSwiper2', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+//   grabCursor:true,
+//   autoplay: {
+//     delay: 5000,
+//   },
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//     type: "fraction",
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+// const swiper3 = new Swiper('.pSwiper3', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+//   grabCursor:true,
+//   autoplay: {
+//     delay: 5000,
+//   },
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//     type: "fraction",
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
