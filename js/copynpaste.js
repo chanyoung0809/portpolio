731a84345ccddc564cdb8b649055cb3188e77fb0
Chanyoung <cisalive@naver.com>
2023-07-17 16:10:59 +0900

    프로필 사진 이미지 애니메이션 작업 완료, 링크 작업 추가 필요.

--- js/copynpaste.js
+++ js/copynpaste.js
@@ -1,5 +1,4 @@
 const texts = document.querySelectorAll(".contacts .contact > p ");
-console.log(texts);
 Array.from(texts).forEach((text, idx)=>{
     text.addEventListener("click", async () => {
         // 클립보드 복사가 비동기적 작업이기 때문에, 비동기 작업 가능하게 하는 async와 지연시켜주는 await 키워드 사용
