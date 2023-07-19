381f7eeb6a412bb7a336e10cf08999054abc967b
Chanyoung <cisalive@naver.com>
2023-07-19 20:22:12 +0900

    포트폴리오 링크작업 및 이미지 작업 완료.

--- js/open.js
+++ js/open.js
@@ -1,3 +1,30 @@
+// 포트폴리오 부분 미디어 쿼리 보기 
+let openWindow = (link, screenwidth, screenheight)=>{
+    let width = screenwidth;
+    let height = screenheight;
+    let left = (window.screen.width - width) / 2;
+    let top = (window.screen.height - height) / 2;
+    let windowFeatures = `width=${width}, height=${height},left=${left},top=${top}`;
+    window.open(link, '_blank', windowFeatures);
+}
+let openTablet = (link, screenwidth, screenheight)=>{
+    let width = 1200;
+    let height = 675;
+    let left = (window.screen.width - width) / 2;
+    let top = (window.screen.height - height) / 2;
+    let windowFeatures = `width=${width}, height=${height},left=${left},top=${top}`;
+    window.open(link, '_blank', windowFeatures);
+}
+// 모바일
+let openPhone = (link, screenwidth, screenheight)=>{
+    let width = 414;
+    let height = 749;
+    let left = (window.screen.width - width) / 2;
+    let top = (window.screen.height - height) / 2;
+    let windowFeatures = `width=${width}, height=${height},left=${left},top=${top}`;
+    window.open(link, '_blank', windowFeatures);
+}
+// 강점 부분 이미지 확대해서 보기
 const imgs = document.querySelectorAll(".talks > img");
 const popup = document.querySelector(".photo");
 const closeBtn = document.querySelector(".photo > .close");
