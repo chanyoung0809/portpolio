a95c02899d01950b96ce87c80053378282243138
chanyoung0809 <cisalive@naver.com>
2023-10-21 00:10:12 +0900

    10/21 수정본, 링크 주소 교체

--- js/copynpaste.js
+++ js/copynpaste.js
@@ -1,9 +1,15 @@
 const texts = document.querySelectorAll(".contacts .contact > p ");
 Array.from(texts).forEach((text, idx)=>{
-    text.addEventListener("click", async () => {
+    text.addEventListener("mouseup", async () => {
         // 클립보드 복사가 비동기적 작업이기 때문에, 비동기 작업 가능하게 하는 async와 지연시켜주는 await 키워드 사용
         await navigator.clipboard.writeText(text.innerText);
         let sort = ['전화번호가', 'URL이', '이메일주소가']
         alert(`${sort[idx]} 클립보드에 복사되었습니다.`);
-    })
+    });
+    text.parentNode.addEventListener("drag", async () => {
+        // 클립보드 복사가 비동기적 작업이기 때문에, 비동기 작업 가능하게 하는 async와 지연시켜주는 await 키워드 사용
+        await navigator.clipboard.writeText(text.innerText);
+        let sort = ['전화번호가', 'URL이', '이메일주소가']
+        alert(`${sort[idx]} 클립보드에 복사되었습니다.`);
+    });
 });
--- js/portfolioViews.js
+++ js/portfolioViews.js
@@ -7,6 +7,28 @@ const contents = document.querySelectorAll(".contents_wrap > .content");
 const backBtns = document.querySelectorAll(".right_content > .backBtn");
 // 세부 콘텐츠 보기-> 슬라이드로 돌아갈 뒤로가기 버튼
 
+window.addEventListener('keyup', (e)=>{
+    //세부 콘텐츠가 보이지 않는 상태일 때 엔터 키를 누르면
+    if (e.key === 'Enter' && !contents_wrap.classList.contains("on")){
+        e.preventDefault(); // 링크이동기능 막고
+        let allslides = document.querySelectorAll(".portfolioSwiper .swiper-slide");
+        let activeSlide = document.querySelector(".portfolioSwiper .swiper-slide.swiper-slide-active");
+        let activeNum = Array.from(allslides).indexOf(activeSlide);
+        contents_wrap.classList.add("on"); // 콘텐츠 감싸는 부모 보이게 함
+        contents.forEach((content,i)=>{ // 반복문으로 모든 콘텐츠 안보이게 하고
+            content.classList.remove("on");
+        });
+        contents[activeNum].classList.add("on"); //해당 콘텐츠만 보이게 함
+    }
+    else if(e.key === 'Enter' && contents_wrap.classList.contains("on")){
+        contents.forEach((content,i)=>{ // 반복문으로 모든 콘텐츠 안보이게 함
+            content.classList.remove("on");
+        });
+        contents_wrap.classList.remove("on"); // 콘텐츠 감싸는 부모 안보이게 함
+    }
+
+});
+
 // 포폴 슬라이드 클릭하면
 Array.from(portfolioSlide).forEach((slide, idx)=>{
     slide.addEventListener('click', (e)=>{
